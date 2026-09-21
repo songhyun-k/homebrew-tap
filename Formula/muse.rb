60600ac836edf7f97bd505c3429bfdc2c974142d
KIM SONGHYUN <songhyun@unspecialty.com>
2026-09-21 17:20:52 +0900

    Release muse 0.3.1

--- Formula/muse.rb
+++ Formula/muse.rb
@@ -1,9 +1,14 @@
 class Muse < Formula
   desc "Apple Music in your terminal"
   homepage "https://github.com/songhyun-k/muse"
-  url "https://github.com/songhyun-k/muse/releases/download/v0.3.0/muse-macos-arm64.tar.gz"
-  sha256 "08b0853f5597b728e9791c7eaeec69bda7a2a07810eccdfd43713a7ac79b3a21"
+  url "https://github.com/songhyun-k/muse/releases/download/v0.3.1/muse-macos-arm64.tar.gz"
+  sha256 "54167632813adf040a1ec6eb35f04d8b580070ccdde592c2f7fcd64ce72223a1"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/songhyun-k/muse/releases/download/v0.3.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f0a7edba7f81b1dc82ea55f2812a9e918008b4e2ac3b53d2ec9e24104abc6471"
+  end
 
   depends_on arch: :arm64
   depends_on macos: :sonoma
